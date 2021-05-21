ce094cc3f4a55b1f2f8916b01755ac7fd65ac195
Gerolf Ziegenhain <gerolfziegenhain@gz-dfs-macbook.mainz.ziegenhain.com>
2021-05-22 00:11:05 +0200

    hiit timer example

--- hangboard-app/main.py
+++ hangboard-app/main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, redirect, url_for, session, request
+
 import time
 import json
 #from hx711 import HX711
 
 app = Flask(__name__) 
+app.secret_key = "test"
+
 
 #try:
 #    hx711 = HX711(
@@ -87,7 +91,6 @@ def index():
 def progress():
     def generate():
         x = 0
-        #x = ex.session["Exercise"][0]["Rest-to-Start"]
 
         while x <= 100:
             yield "data:" + str(x) + "\n\n"
@@ -96,6 +99,45 @@ def progress():
 
     
     return Response(generate(), mimetype= 'text/event-stream')
+
+
+
+@app.route("/test", methods=["GET", "POST"])
+def setup():
+    if request.method == "POST":
+        exercise = int(request.form["exercise"])
+        rest = int(request.form["rest"])
+        sets = int(request.form["sets"])
+
+        session["exercise"] = exercise
+        session["rest"] = rest
+        session["sets"] = sets
+        session["set_counter"] = 0
+
+        return redirect(url_for("rest"))
+    return render_template("home.jinja2")
+
+
+@app.route("/rest")
+def rest():
+    return render_template("rest.jinja2", rest=session["rest"])
+
+
+@app.route("/exercise")
+def exercise():
+    if session["set_counter"] == session["sets"]:
+        return redirect(url_for("completed"))
+    session["set_counter"] += 1
+    return render_template("exercise.jinja2", exercise=session["exercise"])
+
+
+@app.route("/complete")
+def completed():
+    return render_template("complete.jinja2", sets=session["set_counter"])
+
+
+
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
