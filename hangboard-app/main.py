a4dfd1edf30b82474c55c3db25b7e12d6d997162
Gerolf Ziegenhain <gerolfziegenhain@gz-dfs-macbook.mainz.ziegenhain.com>
2021-05-22 23:16:37 +0200

    zmq works

--- hangboard-app/main.py
+++ hangboard-app/main.py
@@ -1,123 +1,16 @@
 from flask import Flask, render_template, request, Response, redirect, url_for, session
 import zmq
-from threading import Thread
 
 import time
-import json
-#from hx711 import HX711
 
 HOST = '127.0.0.1'
 PORT = 9090
-TASK_SOCKET = zmq.Context().socket(zmq.REQ)
+TASK_SOCKET = zmq.Context().socket(zmq.SUB)
 TASK_SOCKET.connect('tcp://{}:{}'.format(HOST, PORT))
+TASK_SOCKET.subscribe("")
+
 app = Flask(__name__) 
 app.secret_key = "test"
-
-
-#try:
-#    hx711 = HX711(
-#    dout_pin=5,
-#    pd_sck_pin=6,
-#    channel='A',
-#    gain=64
-#    )
-#
-#    hx711.reset()   # Before we start, reset the HX711 (not obligate)
-#    measures = hx711.get_raw_data(num_measures=3)
-#finally:
-#    GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
-#
-#print("\n".join(measures))
-
-
-# MQTT
-class Exercise(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-
-        self.filename = "../exercises/test.json" # TODO: as parameter
-        with open(self.filename) as json_file:
-            self.data = json.load(json_file)
-
-        self.session = (self.data["Sessions"][0])
-        self.session_name = self.session["Name"]
-        self.total_exercises = len (self.session["Exercise"])
-        self.current_exercise = 0
-        self.current_exercise_name = "Rest to start"
-
-        self._context = zmq.Context()
-        self._socket = self._context.socket(zmq.REP)
-
-        self.zmq_count = 0
-
-        self.active = True
-
-
-    def run(self):
-        self._socket.bind('tcp://{}:{}'.format(HOST, PORT))
-        self._socket.setsockopt(zmq.RCVTIMEO, 500)
-        while self.active:
-            ev = self._socket.poll(1000)
-            self.zmq_count = self.zmq_count + 1
-            if ev:
-                rec = self._socket.recv_json()
-                self._socket.send_json({"response": "ok", "payload": rec, "ZMQ Counter": self.zmq_count})
-
-    def config_exercise (self):
-        e = self.session["Exercise"][self.current_exercise]
-        self.current_exercise_name = e["Type"]
-        self.current_exercise_reps_counter = 0
-        self.current_exercise_reps_total = e["Reps"]
-        self.current_exercise_duration = e["Counter"]
-        self.current_exercise_counter = 0
-        self.current_exercise_pause_duration = e["Pause"]
-        self.current_exercise_rest_to_start = e["Rest-to-Start"]
-
-    def run_exercise (self): 
-        print ("Run one exercise")
-        self.config_exercise()
-
-        for self.current_exercise_reps_counter in range (1, 1+self.current_exercise_reps_total):
-            start_detected = 1
-            if (start_detected == 1):
-                print ("Start detected")
-                if (e["Type"] == "Hang"):
-                    print ("Hang")
-                    for self.current_exercise_counter in range (0, self.current_exercise_duration+1):
-                        time.sleep (1)
-                elif (e["Type"] == "Maximal Hang"):
-                    print ("Key press?")                    
-                elif (e["Type"] == "Assisted Pull Ups"):
-                    print ("Key press?")                    
-                else:
-                    print ("Key press?")
-
-                print ("Pause")
-                for self.current_exercise_counter in range (0, self.current_exercise_pause_duration+1):
-                    time.sleep (1)
-
-
-
-# Ref: https://gist.github.com/blakebjorn/aab2f989a7b68fa83410153181aabbac
-class Worker(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-        self._context = zmq.Context()
-        self._socket = self._context.socket(zmq.REP)
-        self.active = True
-        self.counter = 0
-
-
-    def run(self):
-        self._socket.bind('tcp://{}:{}'.format(HOST, PORT))
-        self._socket.setsockopt(zmq.RCVTIMEO, 500)
-        while self.active:
-            self.counter = self.counter + 1
-            ev = self._socket.poll(1000)
-            if ev:
-                rec = self._socket.recv_json()
-                self._socket.send_json({"response": "ok", "payload": rec, "counter": self.counter})
-
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -212,15 +105,9 @@ def start():
 
 @app.route("/pause")
 def pause():
-    TASK_SOCKET.send_json({"command": "pause"})
+    #TASK_SOCKET.send_json({"command": "pause"})
     results = TASK_SOCKET.recv_json()
     return f"pausing {results}"
 
 if __name__ == "__main__":
-    #worker = Worker()
-    #worker.start() 
-
-    ex = Exercise()
-    ex.start()
-
     app.run(host="127.0.0.1", port=8080, debug=True)
