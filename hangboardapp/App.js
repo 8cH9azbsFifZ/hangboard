fc54af140fd5213dc782e2bea892fc5b157df616
8cH9azbsFifZ <g@ziegenhain.com>
2021-05-27 21:38:37 +0200

    add-sounds

--- hangboardapp/App.js
+++ hangboardapp/App.js
@@ -33,6 +33,21 @@ var Sound = require('react-native-sound');
 /*
  
 */
+var client = new WebSocket ('ws://127.0.0.1:4321/'); // FIXME
+Sound.setCategory('Playback');
+
+var SFXone = new Sound('1.mp3', Sound.MAIN_BUNDLE);
+var SFXtwo = new Sound('2.mp3', Sound.MAIN_BUNDLE);
+var SFXthree = new Sound('3.mp3', Sound.MAIN_BUNDLE);
+var SFXfour = new Sound('4.mp3', Sound.MAIN_BUNDLE);
+var SFXfive = new Sound('5.mp3', Sound.MAIN_BUNDLE);
+var SFXsix = new Sound('6.mp3', Sound.MAIN_BUNDLE);
+var SFXseven = new Sound('7.mp3', Sound.MAIN_BUNDLE);
+var SFXeight = new Sound('8.mp3', Sound.MAIN_BUNDLE);
+var SFXnine = new Sound('9.mp3', Sound.MAIN_BUNDLE);
+var SFXten = new Sound('10.mp3', Sound.MAIN_BUNDLE);
+var SFXdone = new Sound('done.mp3', Sound.MAIN_BUNDLE);
+var SFXfailed = new Sound('failed.mp3', Sound.MAIN_BUNDLE);
 
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -67,32 +82,37 @@ const App: () => Node = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  var client = new WebSocket ('ws://127.0.0.1:4321/'); // FIXME
   const [myText, setMyText] = useState("My Original Text");
 
   client.onmessage = function(e) {
     if (typeof e.data === 'string') {
       mydata = e.data;
-      setMyText(e.data)
+      //setMyText(e.data)
       console.log("Received: '" + e.data + "'");
     }
+
+    var parsed = JSON.parse(e.data);
+    setMyText(parsed.Counter); //parsed.Counter);
+
+    var togo = parsed.Duration - parsed.Counter;
+
+    if (parsed.TimerStatus == false)
+    {
+      if (togo == 10) { SFXten.play(); } 
+      if (togo == 9) { SFXnine.play(); } 
+      if (togo == 8) { SFXeight.play(); } 
+      if (togo == 7) { SFXseven.play(); } 
+      if (togo == 6) { SFXsix.play(); } 
+      if (togo == 5) { SFXfive.play(); } 
+      if (togo == 4) { SFXfour.play(); } 
+      if (togo == 3) { SFXthree.play(); } 
+      if (togo == 2) { SFXtwo.play(); } 
+      if (togo == 1) { SFXone.play(); } 
+      if (togo == 0) { SFXdone.play(); } 
+    }
+    
   }; 
 
-  Sound.setCategory('Playback');
-
-  var SFXone = new Sound('1.mp3', Sound.MAIN_BUNDLE);
-  var SFXtwo = new Sound('2.mp3', Sound.MAIN_BUNDLE);
-  var SFXthree = new Sound('3.mp3', Sound.MAIN_BUNDLE);
-  var SFXfour = new Sound('4.mp3', Sound.MAIN_BUNDLE);
-  var SFXfive = new Sound('5.mp3', Sound.MAIN_BUNDLE);
-  var SFXsix = new Sound('6.mp3', Sound.MAIN_BUNDLE);
-  var SFXseven = new Sound('7.mp3', Sound.MAIN_BUNDLE);
-  var SFXeight = new Sound('8.mp3', Sound.MAIN_BUNDLE);
-  var SFXnine = new Sound('9.mp3', Sound.MAIN_BUNDLE);
-  var SFXten = new Sound('10.mp3', Sound.MAIN_BUNDLE);
-  var SFXdone = new Sound('done.mp3', Sound.MAIN_BUNDLE);
-  var SFXfailed = new Sound('failed.mp3', Sound.MAIN_BUNDLE);
-  
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -104,10 +124,9 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Backend Exercise">
-    
-          <Text onPress = {() => SFXdone.play()}>
-            {myText}
-          </Text>
+            <Text onPress = {() => SFXdone.play()}>
+              {myText}
+            </Text>
           </Section>
   
         </View>
