4c8714fb573fe08d7dfcfd1836b407accb25b721
Gerolf Ziegenhain <gerolfziegenhain@gz-dfs-macbook.mainz.ziegenhain.com>
2021-05-29 00:38:51 +0200

    tabs

--- hardware/gyroscope/sensor_zlagboard.py
+++ hardware/gyroscope/sensor_zlagboard.py
@@ -187,10 +187,8 @@ while True:
 
 	    print("Angle X: " + str(kalAngleX)+"   " +"Angle Y: " + str(kalAngleY))
 	    #print(str(roll)+"  "+str(gyroXAngle)+"  "+str(compAngleX)+"  "+str(kalAngleX)+"  "+str(pitch)+"  "+str(gyroYAngle)+"  "+str(compAngleY)+"  "+str(kalAngleY))
-	    
+	    ws.send("1234")
         # Detect critical angle
-        ws.send("1234")
-        
         time.sleep(0.005)
 
 	except Exception as exc:
