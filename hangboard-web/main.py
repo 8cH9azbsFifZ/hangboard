77b03ff590c6fd7dd4a5a114401de0daaf6b14d0
Gerolf Ziegenhain <gerolfziegenhain@gz-dfs-macbook.mainz.ziegenhain.com>
2021-05-29 21:28:15 +0200

    remove old deps

--- hangboard-web/main.py
+++ hangboard-web/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, Response, redirect, url_for, session
+from flask import Flask, render_template, request, Response, session
 
 app = Flask(__name__) 
 app.secret_key = "test"
