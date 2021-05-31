c35f69e9f574befdddd147455aaf746538872b40
Gerolf Ziegenhain <gerolfziegenhain@gz-dfs-macbook.mainz.ziegenhain.com>
2021-05-31 22:21:56 +0200

    cleanup

--- hardware/force_sensors/example/hx711py/1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import RPi.GPIO as GPIO  # import GPIO
-from hx711 import HX711  # import the class HX711
-
-GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
-hx = HX711(17, 27)  # create an object
-print(hx.get_raw_data_mean())  # get raw data reading from hx711
-GPIO.cleanup()
