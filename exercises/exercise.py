1d176c11d0abe00991b6190f867b8844b59a2791
Gerolf Ziegenhain <gerolfziegenhain@gz-dfs-macbook.mainz.ziegenhain.com>
2021-06-01 00:45:05 +0200

    doc

--- exercises/exercise.py
+++ exercises/exercise.py
@@ -13,6 +13,7 @@ import threading
 import asyncio
 import websockets
 
+# Parse commandline
 parser = argparse.ArgumentParser(description="Workout Timer Backend.")
 parser.add_argument ('--host')
 parser.add_argument ('--port')
@@ -21,29 +22,18 @@ args = parser.parse_args()
 WSHOST = args.host 
 WSPORT = args.port 
 
-workoutfile = "workout-test.json"
+workoutfile = "workout-test.json" # FIXME
 
 class Workout():
     def __init__(self):
         self.init_workout()
         self.init_board()
-        #Thread.__init__(self)
-        #self.active = True
-        #self.daemon = True
-
-        #self.start()
-
-    def run_sender(self):
-        """
-        print ("start sender")
-        """
-        print ("start sender")
-        self.start_server = websockets.serve(self.exercisestatus, WSHOST, WSPORT)
-        asyncio.get_event_loop().run_until_complete(self.start_server)
-        asyncio.get_event_loop().run_forever()    
     
     def run_handler(self):
-        print ("start handler")
+        """
+        Start the websocket server and wait for input
+        """
+        print ("Start handler")
         self.start_server = websockets.serve(self.handler, WSHOST, WSPORT)
         asyncio.get_event_loop().run_until_complete(self.start_server)
         asyncio.get_event_loop().run_forever()
@@ -56,13 +46,16 @@ class Workout():
             self.boarddata = json.load(json_file)
 
         self.boardname = self.boarddata["Name"]
-        self.boardimagename = "zlagboard_evo.svg"
-        self.holds_active = ["A1", "A7"]
+        self.boardimagename = "zlagboard_evo.svg" ## FIXME
+        self.holds_active = ["A1", "A7"] # FIXME
         self.holds_inactive = ["A2", "A3", "A4", "A5", "A6", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "C1", "C2", "C3", "C4", "C5", "C6", "C7"]
 
         self.get_board()
 
     def init_workout(self):
+        """
+        Initialize a new workout run
+        """
         self.filename = workoutfile
 
         with open(self.filename) as json_file:
@@ -76,39 +69,48 @@ class Workout():
         self.current_set = 0
         self.current_set_name = "Rest to start"
 
-    async def exercisestatus(self, websocket, path):
-        while True:
-            sleept = 1
-            await websocket.send(self.exercise_status)
-            await asyncio.sleep(sleept)
-
     async def consumer_handler(self, websocket, path): # TODO: https://www.w3schools.com/python/python_inheritance.asp
+        """
+        Handler for receicing commands 
+        """
         async for message in websocket:
             print ("Received it:")
             print (message)
             await self.consumer(message)
 
     async def consumer (self, message):
-            print("Received request: %s" % message)
-            if (message == "Start"):
-                self._run_set()
-            if (message == "Stop"):
-                self._stop_set()     
-            if (message == "GetBoard"):
-                self.get_board()
+        """
+        Execute commands as received from websocket (handler)
+        """
+        print("Received request: %s" % message)
+        if (message == "Start"):
+            self._run_set()
+        if (message == "Stop"):
+            self._stop_set()     
+        if (message == "GetBoard"):
+            self.get_board()
 
     def get_board(self):
-        self.holds_active = []
+        """
+        Get the board configuration (STUB)
+        """
+        self.holds_active = [] # FIXME
         self.holds_inactive = ["A1", "A7", "A2", "A3", "A4", "A5", "A6", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "C1", "C2", "C3", "C4", "C5", "C6", "C7"]
         self.exercise_status = json.dumps({"Exercise": "Pause", "Duration": 0, "Counter": 0, "Completed": 0, "HoldsActive": self.holds_active, "HoldsInactive": self.holds_inactive, "BoardName": self.boardname, "BordImageName": self.boardimagename, "TimerStatus": True})
 
     async def producer_handler(self, websocket, path):
+        """
+        Send the current status of the exercise every second via websocket.
+        """
         while True:
-            message = self.exercise_status #await producer()
+            message = self.exercise_status 
             await websocket.send(message)
-            await asyncio.sleep(1) #new
+            await asyncio.sleep(1) 
 
     async def handler(self, websocket, path):
+        """
+        Handler for the websockets: Receiving and Sending
+        """
         consumer_task = asyncio.ensure_future(
             self.consumer_handler(websocket, path))
         producer_task = asyncio.ensure_future(
@@ -122,24 +124,33 @@ class Workout():
             task.cancel()
 
     def run_workout (self):
+        """
+        Run a single workout
+        """
         for w in range (0, self.total_sets+1):
             self.current_set = w
             self.run_set()
 
     def _run_set (self):
+        """
+        Start a workout "set" in a thread
+        """
         print ("Run thread set")
         self.run_set_thread = threading.Thread(target=self.run_set)
         self.run_set_thread.do_stop = False
         self.run_set_thread.start()
 
     def _stop_set (self):
+        """
+        Stop a workout "set" thread" by setting a flag, which must be caputured in "run_set".
+        """
         print ("Stop thread set")
         self.run_set_thread.do_stop = True
 
     def run_set (self):
-    """
-    Run Set Function
-    """
+        """
+        Run Set Function with main logic
+        """
         print ("Run one set")
         t = threading.currentThread()
         e = self.workout["Sets"][self.current_set]
@@ -194,9 +205,9 @@ class Workout():
 
 
 if __name__ == "__main__":
+    """
+    Main Task
+    """
     ex = Workout()
-    #ex.run_sender()
     ex.run_handler()
-    while True:
-        print ("Exercise Timer Backend running")
-        time.sleep(1)
+
