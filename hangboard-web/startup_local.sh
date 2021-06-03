2251afc1c281ff501d038ee6455c2471dd999982
8cH9azbsFifZ <g@ziegenhain.com>
2021-06-03 22:39:26 +0200

    start on all ips

--- hangboard-web/startup_local.sh
+++ hangboard-web/startup_local.sh
@@ -2,4 +2,4 @@
 python3 -m venv venv
 source venv/bin/activate
 #python3 main.py --host 10.101.101.14 --port 8080
-python3 main.py --host 127.0.0.1 --port 8080
+python3 main.py --host 0.0.0.0 --port 8080
