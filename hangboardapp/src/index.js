fc982671bc77b89471805b306adebd7e2704cf7e
Gerolf Ziegenhain <gerolfziegenhain@gz-dfs-macbook.mainz.ziegenhain.com>
2021-06-06 19:02:17 +0200

    web app loads

--- hangboardapp/src/index.js
+++ hangboardapp/src/index.js
@@ -3,8 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './src/app.json';
+import App from './App';
+import {name as appName} from '../app.json';
 
 AppRegistry.registerComponent(appName, () => App);
 AppRegistry.runApplication(appName, {
