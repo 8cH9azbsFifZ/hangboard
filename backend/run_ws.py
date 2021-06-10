9ef8056b01f6890c982b50125e1ca09f7cfa4182
8cH9azbsFifZ <g@ziegenhain.com>
2021-06-10 21:09:03 +0200

    wip_no_thread

--- backend/run_ws.py
+++ backend/run_ws.py
@@ -2,13 +2,6 @@
 Commandline interface to the hangboard - serving websockets
 """
 
-
-from messager import Messager
-from sensor_zlagboard import SensorZlagboard
-from board import Board
-from board import AsciiBoard
-from timers import PauseTimer
-from timers import ExerciseTimer
 from workout import Workout
 
 """
