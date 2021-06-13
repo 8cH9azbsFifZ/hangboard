9e4b805cab8e699a32d90823a855c63639985aed
Gerolf Ziegenhain <gerolfziegenhain@gz-dfs-macbook.mainz.ziegenhain.com>
2021-06-14 00:02:49 +0200

    wip flutter app test

--- flutter_hangboard/lib/main.dart
+++ flutter_hangboard/lib/main.dart
@@ -1,4 +1,13 @@
+import 'dart:async';
+//import 'dart:html';
+import 'dart:io';
+import 'package:audioplayers/audioplayers.dart';
+//import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/foundation.dart';
+
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+//import 'dart:io' show Platform; //at the top
 
 void main() {
   runApp(MyApp());
@@ -11,12 +20,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Hangboard',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: MyHomePage(title: 'Hangboard'),
     );
   }
 }
+
+// https://pub.dev/packages/audioplayers/example
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -28,6 +39,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
+
+  final _channel = WebSocketChannel.connect(
+    //Uri.parse('wss://echo.websocket.org'),
+    Uri.parse('ws://127.0.0.1:4321'),
+  );
+
+  //bool isIOS = Theme.of(context).platform == TargetPlatform.iOS;
+  //String os = dio.Platform.operatingSystem; //in your code
+
+  //dio.isIOS
+
+  _playLocal() async {
+    AudioPlayer audioPlayer = AudioPlayer();
+    AudioCache audioCache = AudioCache();
+
+    int result = await audioPlayer.play("images/done.mp3", isLocal: true);
+
+    /*int result = await audioPlayer.play(
+        "https://luan.xyz/files/audio/nasa_on_a_mission.mp3",
+        isLocal: false);
+  */
+  }
+
   int _counter = 0;
 
   void _incrementCounter() {
@@ -36,31 +71,119 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      _channel.sink.add(_controller.text);
+    }
+    _playLocal();
+  }
+
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          /*1*/
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /*2*/
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  'Hang for 10 seconds',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                'This is my workout',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          ),
+        ),
+        /*3*/
+        Icon(
+          Icons.star,
+          color: Colors.red[500],
+        ),
+        Text('41'),
+      ],
+    ),
+  );
+
+  Widget origSection = Container(
+      child: Row(
+    children: [
+      Expanded(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Form(
+            child: TextFormField(
+              //controller: _controller,
+              decoration: InputDecoration(labelText: 'Send a message'),
+            ),
+          ),
+          SizedBox(height: 24),
+          StreamBuilder(
+            //stream: _channel.stream,
+            builder: (context, snapshot) {
+              return Text(snapshot.hasData ? '${snapshot.data}' : '');
+            },
+          )
+        ],
+      ))
+    ],
+  ));
+
+  @override
+  void dispose() {
+    _channel.sink.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: [
+            Image.asset(
+              'images/board.png',
+              fit: BoxFit.cover,
+              width: 500,
             ),
-            Text(
+            titleSection,
+            origSection,
+            StreamBuilder(
+              stream: _channel.stream,
+              builder: (context, snapshot) {
+                return Text(snapshot.hasData ? '${snapshot.data}' : '');
+              },
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: _sendMessage, //_incrementCounter,
+            tooltip: 'Increment',
+            child: Icon(Icons.send)));
+  }
+}
+
+/*
+    Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+
+
+
+
+*/
