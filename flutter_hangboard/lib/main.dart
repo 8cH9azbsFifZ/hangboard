31feeb08b1d92621615c6c42ded559b429bc6aec
Gerolf Ziegenhain <gerolfziegenhain@gz-dfs-macbook.mainz.ziegenhain.com>
2021-06-17 22:11:52 +0200

    dynamic display hack working for load

--- flutter_hangboard/lib/main.dart
+++ flutter_hangboard/lib/main.dart
@@ -38,8 +38,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _controller = TextEditingController();
-
   Widget titleSection = Container(
     padding: const EdgeInsets.all(32),
     child: Row(
@@ -130,10 +128,6 @@ class _MyHomePageState extends State<MyHomePage> {
           //origSection,
         ],
       ),
-      /*floatingActionButton: FloatingActionButton(
-            onPressed: _sendMessage, //_incrementCounter,
-            tooltip: 'Send a Command to Backend',
-            child: Icon(Icons.send))*/
     );
   }
 }
@@ -373,44 +367,231 @@ class _LineChartSample2State extends State<LineChartSample2> {
     const Color(0x0000ffff),
   ];
 
-  bool showAvg = false;
+  bool showAvg = false; // FIXME
+
+  final _channel = WebSocketChannel.connect(
+    // FIXME: global varible?
+    Uri.parse('ws://10.101.40.40:4321'),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        AspectRatio(
-          aspectRatio: 5,
-          child: Container(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  right: 0.0, left: 0.0, top: 0, bottom: 0),
-              child: LineChart(
-                showAvg ? avgData() : mainData(),
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 60,
-          height: 34,
-          child: TextButton(
-            onPressed: () {
-              setState(() {
-                showAvg = !showAvg;
-              });
-            },
-            child: Text(
-              'avg',
-              style: TextStyle(
-                  fontSize: 12,
-                  color:
-                      showAvg ? Colors.white.withOpacity(0.5) : Colors.white),
-            ),
-          ),
-        ),
-      ],
-    );
+    return Container(
+        child: StreamBuilder(
+            stream: _channel.stream,
+            builder: (context, snapshot) {
+              var testchen = "Nix";
+              var mytimes;
+              var myload;
+              // Code here
+              String CurrentMeasurementsSeries = "";
+              List<FlSpot> mydata = [];
+              if (snapshot.hasData) {
+                testchen = snapshot.data.toString();
+                Map<String, dynamic> ok1 = jsonDecode(testchen);
+                if (ok1.containsKey("CurrentMeasurementsSeries")) {
+                  //Map<String, dynamic> ok2 =
+                  //  jsonDecode(ok1["CurrentMeasurementsSeries"]);
+                  mytimes = ok1["CurrentMeasurementsSeries"]["time"];
+                  myload = ok1["CurrentMeasurementsSeries"]["load"];
+                  for (int i = 0; i < mytimes.length; i++) {
+                    mydata.add(FlSpot(
+                      double.parse(mytimes[i].toString()),
+                      double.parse(myload[i].toString()),
+                    )); //double.parse(mytimes[i]), double.parse(myload[i])));
+                  }
+                }
+
+                /*
+                CurrentMeasurementsSeries =
+                    ok1['CurrentMeasurementsSeries'] != null
+                        ? (ok1['CurrentMeasurementsSeries']).toString()
+                        : "None";
+                //   var SeriesTime = CurrentMeasurementsSeries['time'];
+                //   var SeriesLoad = CurrentMeasurementsSeries['load'];
+                var timesJson = jsonDecode(CurrentMeasurementsSeries)["time"];
+                //   List<String> times =
+                //    timesJson != null ? List.from(timesJson) : null;
+                */
+              }
+
+/*
+Example JSON to list
+ String arrayText = '{"tags": ["dart", "flutter", "json"]}';
+
+  var tagsJson = jsonDecode(arrayText)['tags'];
+  List<String> tags = tagsJson != null ? List.from(tagsJson) : null;
+
+
+*/
+
+              /* Example data
+              Workout(MainThread) server > Frame(fin=True, opcode=<Opcode.TEXT: 1>, 
+              data=b'{"Exercise": "Hang", "Type": "Hang", "Left": "A1", "Right": "A7", "Counter": "10.00", "CurrentCounter": "1.20", 
+              "Completed": "12", "Rest": "8.80", "HangChangeDetected": "", "HangDetected": true, 
+              "FTI": 102.277591006134, "AverageLoad": 7.7173083760531505, "MaximalLoad": 8.356523627660334, "RFD": 3.9736694610795626,
+               "LoadLoss": 0.01636598734691086, 
+               "CurrentMeasurementsSeries": {"time": [1623957336.5194511, 1623957336.6248372, 1623957336.739917, 1623957336.8615391, 1623957336.9793222, 1623957337.08865, 1623957337.201374, 1623957337.310876, 1623957337.4224918, 1623957337.53201, 1623957337.6437378, 1623957337.753417, 1623957337.865253], "load": [6.372050120907403, 6.790819325251163, 6.943099035921621, 7.0280967457141665, 7.4415889303812275, 7.831522991491597, 8.19067443782122, 8.356523627660334, 8.35520437274116, 8.345906766644118, 8.215740281285541, 8.234021385165535, 8.219760867705883]}}', rsv1=False, rsv2=False, rsv3=False)
+
+*/
+/* Debugging receive
+              String abd = "1324";
+              abd = mytimes.toString();
+              return (Text(abd));
+
+              */
+              if (mydata.length == 0) {
+                return Text("No Hang");
+              }
+
+              return (Stack(
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 5,
+                    child: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            right: 0.0, left: 0.0, top: 0, bottom: 0),
+                        child: LineChart(
+                          /* Old stuff
+                          showAvg ? simData() : mainData(),
+                          */
+
+                          LineChartData(
+                            gridData: FlGridData(
+                              // Grid
+                              show: true,
+                              drawVerticalLine: true,
+                              getDrawingHorizontalLine: (value) {
+                                // Grid Horizontal
+                                return FlLine(
+                                  color: const Color(0xff37434d),
+                                  strokeWidth: 1,
+                                );
+                              },
+                              getDrawingVerticalLine: (value) {
+                                // Grid Vertical
+                                return FlLine(
+                                  color: const Color(0xff37434d),
+                                  strokeWidth: 1,
+                                );
+                              },
+                            ),
+                            titlesData: FlTitlesData(
+                              // X Axis
+                              show: true,
+                              bottomTitles: SideTitles(
+                                showTitles: true,
+                                reservedSize: 22,
+                                getTextStyles: (value) => const TextStyle(
+                                    color: Color(0xff68737d),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16),
+                                getTitles: (value) {
+                                  // X Axis description
+                                  /*
+            switch (value.toInt()) {
+              case 2:
+                return 'MAR';
+              case 5:
+                return 'JUN';
+              case 8:
+                return 'SEP';
+            }*/
+                                  return value.toString(); //'';
+                                },
+                                margin: 8,
+                              ),
+                              leftTitles: SideTitles(
+                                // Y Axis
+                                showTitles: true,
+                                getTextStyles: (value) => const TextStyle(
+                                  color: Color(0xff67727d),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                                getTitles: (value) {
+                                  switch (value.toInt()) {
+                                    case 10:
+                                      return '10';
+                                    case 20:
+                                      return '20';
+                                    case 30:
+                                      return '30';
+                                    case 40:
+                                      return '40';
+                                    case 50:
+                                      return '50';
+                                    case 60:
+                                      return '60';
+                                    case 70:
+                                      return '70';
+                                    case 80:
+                                      return '80';
+                                    case 90:
+                                      return '90';
+                                  }
+                                  return '';
+                                  //return value.toString();
+                                },
+                                reservedSize: 28,
+                                margin: 12,
+                              ),
+                            ),
+                            borderData: FlBorderData(
+                                show: true,
+                                border: Border.all(
+                                    color: const Color(0xff37434d), width: 1)),
+                            //minX: 0, // Define extrema if needed
+                            //maxX: 10,
+                            minY: 0,
+                            maxY: 90,
+
+                            lineBarsData: [
+                              LineChartBarData(
+                                spots: mydata,
+                                //isCurved: true,
+                                colors: gradientColors,
+                                barWidth: 5,
+                                //  isStrokeCapRound: true,
+                                dotData: FlDotData(
+                                  show: false,
+                                ),
+                                belowBarData: BarAreaData(
+                                  show: true,
+                                  colors: gradientColors
+                                      .map((color) => color.withOpacity(0.3))
+                                      .toList(),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 60,
+                    height: 34,
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          showAvg = !showAvg;
+                        });
+                      },
+                      child: Text(
+                        'avg',
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: showAvg
+                                ? Colors.white.withOpacity(0.5)
+                                : Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ));
+            }));
   }
 
   LineChartData mainData() {
@@ -576,19 +757,21 @@ class _LineChartSample2State extends State<LineChartSample2> {
     );
   }
 
-  LineChartData avgData() {
+  LineChartData simData() {
     return LineChartData(
-      lineTouchData: LineTouchData(enabled: false),
       gridData: FlGridData(
+        // Grid
         show: true,
-        drawHorizontalLine: true,
-        getDrawingVerticalLine: (value) {
+        drawVerticalLine: true,
+        getDrawingHorizontalLine: (value) {
+          // Grid Horizontal
           return FlLine(
             color: const Color(0xff37434d),
             strokeWidth: 1,
           );
         },
-        getDrawingHorizontalLine: (value) {
+        getDrawingVerticalLine: (value) {
+          // Grid Vertical
           return FlLine(
             color: const Color(0xff37434d),
             strokeWidth: 1,
@@ -596,6 +779,7 @@ class _LineChartSample2State extends State<LineChartSample2> {
         },
       ),
       titlesData: FlTitlesData(
+        // X Axis
         show: true,
         bottomTitles: SideTitles(
           showTitles: true,
@@ -605,6 +789,8 @@ class _LineChartSample2State extends State<LineChartSample2> {
               fontWeight: FontWeight.bold,
               fontSize: 16),
           getTitles: (value) {
+            // X Axis description
+            /*
             switch (value.toInt()) {
               case 2:
                 return 'MAR';
@@ -612,12 +798,13 @@ class _LineChartSample2State extends State<LineChartSample2> {
                 return 'JUN';
               case 8:
                 return 'SEP';
-            }
-            return '';
+            }*/
+            return value.toString(); //'';
           },
           margin: 8,
         ),
         leftTitles: SideTitles(
+          // Y Axis
           showTitles: true,
           getTextStyles: (value) => const TextStyle(
             color: Color(0xff67727d),
@@ -626,14 +813,27 @@ class _LineChartSample2State extends State<LineChartSample2> {
           ),
           getTitles: (value) {
             switch (value.toInt()) {
-              case 1:
-                return '10k';
-              case 3:
-                return '30k';
-              case 5:
-                return '50k';
+              case 10:
+                return '10';
+              case 20:
+                return '20';
+              case 30:
+                return '30';
+              case 40:
+                return '40';
+              case 50:
+                return '50';
+              case 60:
+                return '60';
+              case 70:
+                return '70';
+              case 80:
+                return '80';
+              case 90:
+                return '90';
             }
             return '';
+            //return value.toString();
           },
           reservedSize: 28,
           margin: 12,
@@ -642,43 +842,87 @@ class _LineChartSample2State extends State<LineChartSample2> {
       borderData: FlBorderData(
           show: true,
           border: Border.all(color: const Color(0xff37434d), width: 1)),
-      minX: 0,
-      maxX: 11,
+      //minX: 0, // Define extrema if needed
+      //maxX: 10,
       minY: 0,
-      maxY: 6,
+      maxY: 90,
+
       lineBarsData: [
         LineChartBarData(
           spots: [
-            FlSpot(0, 3.44),
-            FlSpot(2.6, 3.44),
-            FlSpot(4.9, 3.44),
-            FlSpot(6.8, 3.44),
-            FlSpot(8, 3.44),
-            FlSpot(9.5, 3.44),
-            FlSpot(11, 3.44),
+            FlSpot(14.59, 2.46),
+            FlSpot(14.67, 3.63),
+            FlSpot(14.76, 5.90),
+            FlSpot(14.85, 9.80),
+            FlSpot(14.94, 15.94),
+            FlSpot(15.03, 23.02),
+            FlSpot(15.12, 29.22),
+            FlSpot(15.20, 35.57),
+            FlSpot(15.30, 41.35),
+            FlSpot(15.38, 46.14),
+            FlSpot(15.47, 51.14),
+            FlSpot(15.56, 55.79),
+            FlSpot(15.65, 58.96),
+            FlSpot(15.73, 62.61),
+            FlSpot(15.82, 67.10),
+            FlSpot(15.91, 71.52),
+            FlSpot(16.00, 74.99),
+            FlSpot(16.08, 75.38),
+            FlSpot(16.17, 74.36),
+            FlSpot(16.26, 73.40),
+            FlSpot(16.35, 72.63),
+            FlSpot(16.44, 72.16),
+            FlSpot(16.53, 72.15),
+            FlSpot(16.61, 71.99),
+            FlSpot(16.70, 71.80),
+            FlSpot(16.79, 72.05),
+            FlSpot(16.88, 72.37),
+            FlSpot(16.96, 72.58),
+            FlSpot(17.05, 72.99),
+            FlSpot(17.14, 73.44),
+            FlSpot(17.23, 73.69),
+            FlSpot(17.31, 73.83),
+            FlSpot(17.40, 73.75),
+            FlSpot(17.49, 73.66),
+            FlSpot(17.58, 73.96),
+            FlSpot(17.67, 74.25),
+            FlSpot(17.76, 74.12),
+            FlSpot(17.84, 73.76),
+            FlSpot(17.93, 73.48),
+            FlSpot(18.02, 72.65),
+            FlSpot(18.11, 70.01),
+            FlSpot(18.19, 64.10),
+            FlSpot(18.28, 51.73),
+            FlSpot(18.37, 37.66),
+            FlSpot(18.46, 26.44),
+            FlSpot(18.55, 16.02),
+            FlSpot(18.64, 9.02),
+            FlSpot(18.72, 4.72),
+            FlSpot(18.81, 2.04),
+            FlSpot(22.89, 2.74),
+            FlSpot(22.98, 4.59),
+            FlSpot(23.07, 7.57),
           ],
-          isCurved: true,
-          colors: [
-            ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                .lerp(0.2)!,
-            ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                .lerp(0.2)!,
-          ],
+          //isCurved: true,
+          colors: gradientColors,
           barWidth: 5,
-          isStrokeCapRound: true,
+          //  isStrokeCapRound: true,
           dotData: FlDotData(
             show: false,
           ),
-          belowBarData: BarAreaData(show: true, colors: [
-            ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                .lerp(0.2)!
-                .withOpacity(0.1),
-            ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                .lerp(0.2)!
-                .withOpacity(0.1),
-          ]),
+          belowBarData: BarAreaData(
+            show: true,
+            colors:
+                gradientColors.map((color) => color.withOpacity(0.3)).toList(),
+          ),
         ),
       ],
     );
+  }
+
+  @override
+  void dispose() {
+    _channel.sink.close();
+    super.dispose();
   }
 }
