d549a06bf1813631e81dfbf76f0b851370ab29d3
Gerolf Ziegenhain <gerolfziegenhain@gz-dfs-macbook.mainz.ziegenhain.com>
2021-06-27 20:54:58 +0200

    add startup to run_ws.py

--- backend/run_ws.py
+++ backend/run_ws.py
@@ -3,20 +3,8 @@ Commandline interface to the hangboard - serving websockets
 """
 
 from workout import Workout
-from websocket_handler import WebsocketHandler
-from mqtt_handler import MQTT_Handler
 
-USE_WEBSOCKETS = False
-USE_MQTT = not USE_WEBSOCKETS
 
-if USE_WEBSOCKETS:
-    class Hangboard(Workout, WebsocketHandler):
-        def __init__(self, verbose=None):
-            super(Hangboard, self).__init__()
-if USE_MQTT:
-    class Hangboard(Workout, MQTT_Handler):
-        def __init__(self, verbose=None):
-            super(Hangboard, self).__init__()
     
 
 """
@@ -24,13 +12,6 @@ The main loop is used for testing currently.
 """
 if __name__ == "__main__":
     print ("Starting")
-    wa = Hangboard()
-    if USE_WEBSOCKETS:
-        wa.run_handler(hostname="localhost", port=4321)
-    if USE_MQTT:
-        wa.run_handler(hostname="localhost", port=1883)
-        # FIXME
+    wa = Workout(hostname="hangboard")
+    wa._core_loop()
 
-    #wa.show_workout()
-    #wa._run_workout()
-    #wa.run_websocket_handler()
