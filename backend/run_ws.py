cb878b79f0b50b962f710601d43ae31ce5cc3b22
8cH9azbsFifZ <g@ziegenhain.com>
2021-06-28 19:25:14 +0200

    store and get maxload

--- backend/run_ws.py
+++ backend/run_ws.py
@@ -11,7 +11,7 @@ The main loop is used for testing currently.
 if __name__ == "__main__":
     print ("Starting")
     wa = Workout(hostname="hangboard")
-    wa._set_workout(id="HRST-S-1-4ZBEVO")
-    #wa._set_workout(id="ZB-A-1")
+    #wa._set_workout(id="HRST-S-1-4ZBEVO")
+    wa._set_workout(id="ZB-A-1")
     wa._core_loop()
 
