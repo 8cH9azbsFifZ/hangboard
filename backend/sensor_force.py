479c0390d62ea22f9601d9afd0af8a4832ea779d
8cH9azbsFifZ <g@ziegenhain.com>
2021-06-30 21:52:38 +0200

    lpf

--- backend/sensor_force.py
+++ backend/sensor_force.py
@@ -241,7 +241,7 @@ class SensorForce():
         if len(self._moving_average_series) >= self._moving_average_n: 
             self._moving_average_series.pop() # restrict size of array for moving average   
             self._moving_average_load = uniform_filter1d(self._moving_average_series, size=self._moving_average_n)
-            return self._moving_average_load[self._moving_average_n]
+            return self._moving_average_load[self._moving_average_n-1]
 
         return 0
 
