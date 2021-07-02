ba717a261e384d5d667d6f682724ca6f69789740
8cH9azbsFifZ <g@ziegenhain.com>
2021-07-02 20:35:44 +0200

    moved distance sensor to backend

--- backend/sensor_distance.py
+++ backend/sensor_distance.py
@@ -1,74 +1,76 @@
-#Bibliotheken einbinden
+"""
+Class containing all stuff for the distance sensor
+"""
+
 import RPi.GPIO as GPIO
 import time
  
-#GPIO Modus (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
+class SensorDistance():
+    def __init__(self, pin_trigger=18, pin_echo=24):
+        #GPIO Mode (BOARD / BCM)
+        GPIO.setmode(GPIO.BCM)
  
-#GPIO Pins zuweisen
-GPIO_TRIGGER = 18
-GPIO_ECHO = 24
+        #set GPIO Pins 
+        self._GPIO_TRIGGER = pin_trigger
+        self._GPIO_ECHO = pin_echo
 
-#Richtung der GPIO-Pins festlegen (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+        #Set GPIO pin modes: in / out
+        GPIO.setup(self._GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(self._GPIO_ECHO, GPIO.IN)
 
-R = 40
-H = 1
-Q = 10
-P = 0
-U_hat = 0
-K = 0
+        self._R = 40
+        self._H = 1
+        self._Q = 10
+        self._P = 0
+        self._U_hat = 0
+        self._K = 0
 
-def kalman(U):
-    global R
-    global P
-    global H
-    global Q
-    global U_hat
-    K = P*H/(H*P*H+R)
-    U_hat += + K*(U-H*U_hat)
-    P = (1-K*H)*P+Q
-    return U_hat
+    def kalman(self, U):
+        self._K = self._P*self._H/(self._H*self._P*self._H+self._R)
+        self._U_hat += + self._K*(U-self._H*self._U_hat)
+        self._P = (1-self._K*self._H)*self._P+self._Q
+        return self._U_hat
+
+    def distance(self):
+        # setze Trigger auf HIGH
+        GPIO.output(self._GPIO_TRIGGER, True)
+    
+        # setze Trigger nach 0.01ms aus LOW
+        time.sleep(0.00001)
+        GPIO.output(self._GPIO_TRIGGER, False)
+    
+        tstart = time.time()
+        tstop = time.time()
+    
+        # store start time
+        while GPIO.input(self._GPIO_ECHO) == 0:
+            tstart = time.time()
+    
+        # store stop time
+        while GPIO.input(self._GPIO_ECHO) == 1:
+            tstop = time.time()
+    
+        TimeElapsed = tstop - tstart
+        # multiply by velocity of sound (34300 cm/s) 
+        # and divide by two (forth and back)
+        distance = (TimeElapsed * 34300) / 2
+    
+        return distance
+ 
+    def Stop(self):
+        GPIO.cleanup()
 
 
-def distanz():
-    # setze Trigger auf HIGH
-    GPIO.output(GPIO_TRIGGER, True)
- 
-    # setze Trigger nach 0.01ms aus LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
- 
-    StartZeit = time.time()
-    StopZeit = time.time()
- 
-    # speichere Startzeit
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartZeit = time.time()
- 
-    # speichere Ankunftszeit
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopZeit = time.time()
- 
-    # Zeit Differenz zwischen Start und Ankunft
-    TimeElapsed = StopZeit - StartZeit
-    # mit der Schallgeschwindigkeit (34300 cm/s) multiplizieren
-    # und durch 2 teilen, da hin und zurueck
-    distanz = (TimeElapsed * 34300) / 2
- 
-    return distanz
- 
 if __name__ == '__main__':
+    d = SensorDistance()
     try:
         while True:
-            abstand = distanz()
-            if (abstand < 5000):
-                fabs = kalman (abstand)
-                print ("Gemessene Entfernung = %.1f cm und %.1f" % (abstand, fabs))
+            distance = d.distance()
+            if (distance < 5000):
+                fabs = d.kalman (distance)
+                print ("Distance = %.1f cm and %.1f cm with kalman" % (distance, fabs))
             time.sleep(.005)
  
-        # Beim Abbruch durch STRG+C resetten
     except KeyboardInterrupt:
-        print("Messung vom User gestoppt")
-        GPIO.cleanup()
+        print("Done")
+        d.Stop()
