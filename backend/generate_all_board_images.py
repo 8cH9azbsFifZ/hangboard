345dc93dce014ae5622d2e0a55ffa0f893765a87
8cH9azbsFifZ <g@ziegenhain.com>
2021-07-03 00:40:28 +0200

    #98

--- backend/generate_all_board_images.py
+++ backend/generate_all_board_images.py
@@ -3,5 +3,5 @@ from board import Board, SVGBoard
 if __name__ == "__main__":
     a = Board()
     svg = SVGBoard(boardname="zlagboard_evo")
-    svg.generate_all_images(holds=a.all_holds) # FIXME: all holds images must be genearted and put into assets directory of flutter app.. #83
-    # FIXME: not all images are generated #83
+    svg.generate_all_images(holds=a.all_holds) 
+    # FIXME: all holds images must be genearted and put into assets directory of flutter app.. #83
