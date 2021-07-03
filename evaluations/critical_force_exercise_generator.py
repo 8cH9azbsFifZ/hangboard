2bb59cc01e9cb9298c3f6fa17464e311f3498d17
Gerolf Ziegenhain <gerolfziegenhain@gz-dfs-macbook.mainz.ziegenhain.com>
2021-07-03 20:45:49 +0200

    description for critical force calculation

---
+++ evaluations/critical_force_exercise_generator.py
@@ -0,0 +1,50 @@
+"""
+Ref: https://strengthclimbing.com/critical-force-calculator/#footnote_1_5886
+
+Critical Force Calculator
+The Critical Force Calculator will help you determine where you stand with your forearm aerobic endurance. If you’re a strong sport’s or trad climber, but end up pumped on your way to the crux, it means that your forearm aerobic endurance likely requires some attention. The Critical Force Calculator will also allow you to find the optimum load at which you should train so that you will soon breeze up through those endurance routes! 
+
+The calculator provides a basic assessment of your climbing endurance level. If you would like to get a detailed personal climbing performance evaluation like this one, just send your input numbers to me directly at contact@strengthclimbing.com, and I’ll get back to you with an in-depth lead climbing level evaluation. I will analyze your current performance and give you hints on what to train to quickly improve your climbing!
+
+
+Instructions
+To determine your forearm aerobic endurance Critical Force, you can use the calculator below. Input your body weight and your 7-second MVC on a 20 mm edge, followed by respective times to failure for the 7/3 Endurance Repeaters at 80% MVC-7 and 60% MVC-7. For the last measurement you may choose 45%, 50% or 55% MVC-7 load, just make sure that you fail in under 1200 seconds. 
+
+The calculator will output your CF and the weight you need to subtract in order to exercise exactly at CF load. You will also receive a brief assessment of your strength and forearm aerobic endurance. The calculator is calibrated for the half crimp grip on a 20 mm edge, to enable a direct comparison with the results published in [1]. The bouldering level prediction is based on the results of the survey published at [2].
+
+You can find more detailed information regarding the role of the Critical Force in sport’s and climbing endurance training in [3].
+
+Example Critical Force test
+Let’s say you weigh 68 kg, and you take the MVC-7 test on a 20 mm edge. You can add 32 kg to the bodyweight and hang for 7 seconds, so the MVC-7 load is 100 kg. Now we can calculate your 80%, 60% and 45% loads, which are 80 kg, 60 kg and 45 kg respectively.
+
+Test your T80% :
+To set the 80% load, you must add 12 kg to the harness.
+Execute a set of 7/3 Repeaters until failure.
+Add the hanging times together. This means that if you were able to execute 10 full hangs and failed at hang 11 in the 4-th second, then your T80% is 74 seconds.
+Rest 15 minutes before the next test.
+Test your T60%
+To set the 60% load, you must remove 8 kg using a pulley system.
+Execute a set of 7/3 Repeaters until failure.
+Add the hanging times together. This means that if you were able to execute 15 full hangs and failed at hang 16 in the 4-th second, then your T60% is 109 seconds.
+Rest 30 minutes before the next test.
+Test your T45%:
+To set the 45% load, you must remove 23 kg using a pulley system.
+Execute a set of 7/3 Repeaters until failure.
+Add the hanging times together. This means that if you were able to execute 60 full hangs and failed at hang 61 in the 4-th second, then your T45% is 424 seconds.
+If your aerobic endurance is very good, and you are able to last more than 20 minutes at the 45% load, you should change the load to 50% or even 55% of your MVC-7 and redo the test.
+Enter your data into the form and press calculate.
+
+The idea of Critical Force
+Critical Speed (CS) and Critical Power (CP) are well-known concepts used in sports training. Imagine yourself walking at about 6 km/h – you could probably carry on like this for hours. On the other hand, unless you’re an elite marathon runner, you probably couldn’t run at 20 km/h for very long. This means that your running critical speed, which is the speed at which you could theoretically run for an unlimited time, lies somewhere between 6 – 20 km/h. Cycling coaches use the term Critical Power, which defines the power output, at which you could cycle for hours. For climbing, the term Critical Force (CF) is more relevant, meaning the maximum load at which you should be able to climb indefinitely without falling off.
+
+The Critical Force determines the performance on endurance routes, and it is directly related to the forearm aerobic capacity. Critical Force is usually expressed as a percentage of the climber’s body weight, or their maximum voluntary contraction (MVC). For elite sport climbers, it could reach up to 60% of their MVC, while for boulderers and poorly conditioned route climbers, it could be below 30% MVC. Knowing your CF is important because it was shown that aerobic capacity is trained most efficiently exactly at critical loads [4].
+
+References
+Giles, D., Chidley, J.B., Taylor, N., Torr, O., Hadley, J., Randall, T., Fryer, S., 2019. The Determination of Finger-Flexor Critical Force in Rock Climbers. International Journal of Sports Physiology and Performance 1–8. (link)↩
+https://toclimb8a.shinyapps.io/maxtograde/ (link)↩
+J. Banaszczyk, StrengthClimbing – Endurance Repeaters, May 2, 2019. (link)↩
+McGawley, K., 2010. The application of the critical power construct to endurance exercise. University of Brighton. Student thesis: Doctoral Thesis. (link)↩
+
+
+
+"""
