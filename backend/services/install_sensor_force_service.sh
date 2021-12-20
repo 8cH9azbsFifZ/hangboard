b8f891baf28971a75d928037a002d55a5506da3c
Gerolf Ziegenhain <g@ziegenhain.com>
2021-12-20 23:38:31 +0100

    services

---
+++ backend/services/install_sensor_force_service.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+service="sensor_force.service"
+
+echo "Install hangboard service for: "$service 
+sudo cp ./$service /lib/systemd/system/
+sudo chmod 644 /lib/systemd/system/$service
+sudo systemctl daemon-reload
+sudo systemctl enable $service
+sudo systemctl restart $service
