71d9b0fad7f222d7357f9c282af70ad1ead35b65
8cH9azbsFifZ <g@ziegenhain.com>
2021-12-23 22:53:42 +0100

    fix

--- backend/display_hd44780/display.py
+++ backend/display_hd44780/display.py
@@ -13,7 +13,6 @@ logging.basicConfig(level=logging.DEBUG,
 
 class Database():
     def __init__(self):
-
         self.lcd = CharLCD('PCF8574', 0x27, backlight_enabled=True, charmap='A02')
 
 
@@ -32,21 +31,16 @@ class Database():
         self._client.connect(hostname,port,60)#connect
 
         # FIXME: subscribe to all?
-        self._client.subscribe("hangboard/workout/holds")
-        self._client.subscribe("hangboard/workout/workoutlist")
-        self._client.subscribe("hangboard/workout/setinfo")
-        self._client.subscribe("hangboard/workout/timerstatus")
-        self._client.subscribe("hangboard/workout/status")
-        self._client.subscribe("hangboard/workout/workoutstatus")
+        
         self._client.subscribe("hangboard/sensor/load/loadstatus")
-        self._client.subscribe("hangboard/sensor/sensorstatus")
-        self._client.subscribe("hangboard/sensor/lastexercise")
-        self._client.subscribe("hangboard/workout/userstatistics")
-        self._client.subscribe("hangboard/workout/upcoming")
+        #self._client.subscribe("hangboard/sensor/sensorstatus")
+        #self._client.subscribe("hangboard/sensor/lastexercise")
+        #self._client.subscribe("hangboard/workout/userstatistics")
+        #self._client.subscribe("hangboard/workout/upcoming")
 
         self._client.loop_forever()
 
 
 if __name__ == "__main__":
-    d = Database(hostname="raspi-hangboard", user="root", password="rootpassword")
+    d = Database()
     d._record_data(hostname="raspi-hangboard")        
