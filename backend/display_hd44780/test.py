272031f413e0a629cc937466187d45f9ad00dbe8
8cH9azbsFifZ <g@ziegenhain.com>
2021-12-23 22:27:49 +0100

    test

--- backend/display_hd44780/test.py
+++ backend/display_hd44780/test.py
@@ -1,3 +1,14 @@
 from RPLCD.i2c import CharLCD
 lcd = CharLCD('PCF8574', 0x27)
 lcd.write_string('Hello world')
+smiley = (
+    0b00000,
+    0b01010,
+    0b01010,
+    0b00000,
+    0b10001,
+    0b10001,
+    0b01110,
+    0b00000,
+)
+lcd.create_char(0, smiley)
