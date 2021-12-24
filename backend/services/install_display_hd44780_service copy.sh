530e63be1db209f4a7181a37f50715c4358a827b
8cH9azbsFifZ <g@ziegenhain.com>
2021-12-24 22:38:26 +0100

    rights

--- backend/services/install_display_hd44780_service copy.sh
+++ backend/services/install_display_hd44780_service copy.sh
@@ -3,8 +3,6 @@ service="hangboard_display_hd44780.service"
 
 echo "Install hangboard service for: "$service 
 
-sudo pip3 install scipy # FIXME
-
 sudo cp ./$service /lib/systemd/system/
 sudo chmod 644 /lib/systemd/system/$service
 sudo systemctl daemon-reload
