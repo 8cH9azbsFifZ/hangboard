86e5474273699a83ea0b94458dac5c7453ae7960
Gerolf Ziegenhain <g@ziegenhain.com>
2021-12-30 20:53:21 +0100

    cleanup

--- backend/api/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker run --rm -it \
--v $PWD/asyncapi.yaml:/app/asyncapi.yml \
--v $PWD/out:/app/output \
-asyncapi/generator  -o /app/output /app/asyncapi.yml @asyncapi/html-template 
