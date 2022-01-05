69441e17ed77fb384f3ddd149e88c060b8f0a24e
Gerolf Ziegenhain <g@ziegenhain.com>
2022-01-06 00:40:42 +0100

    web api

---
+++ boards/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, json, send_file
+from board import Board
+
+boardname = "zlagboard_evo" # FIXME: config
+a = Board(boardname=boardname)
+
+api = Flask(__name__)
+
+@api.route('/board/data', methods=['GET'])
+def get_board():
+  return json.dumps(a.boarddata)
+
+@api.route('/board/get_hold_for_type/<type>', methods=['GET'])
+def get_hold_for_type(type):
+  return json.dumps(a.get_hold_for_type(type))
+
+@api.route('/board/img/<left>/<right>', methods=['GET']) # FIXME generate all images in advance
+def get_img_left_right(left,right):
+  fname = a.svg._cache_png_filename(left,right)
+  return send_file(fname, mimetype='image/png')
+
+@api.route('/board/img', methods=['GET']) # FIXME: slashes safe?
+def get_img():
+  fname = a.svg.boardimagename_png
+  return send_file(fname, mimetype='image/png')
+
+if __name__ == '__main__':
+    api.run() 
