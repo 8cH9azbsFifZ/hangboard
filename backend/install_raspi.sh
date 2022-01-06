3d3d67eb377e0e92e7600aaf5afd9595bcfd65cb
8cH9azbsFifZ <g@ziegenhain.com>
2022-01-07 00:55:08 +0100

    installation

--- backend/install_raspi.sh
+++ backend/install_raspi.sh
@@ -4,19 +4,18 @@
 sudo apt-get -y install git
 
 # Download the software
-test -e hangboard ||git clone https://github.com/8cH9azbsFifZ/hangboard.git
-cd hangboard
-git pull
+#test -e hangboard ||git clone https://github.com/8cH9azbsFifZ/hangboard.git
+#cd hangboard
+#git pull
 
-./backend/install_01_prepare_raspi.sh
+#./backend/install_01_prepare_raspi.sh
 
 
 # Install the python libraries
-cd backend
-python3 -m pip install -r backend/requirements.txt
+#cd backend
+#
 
 
-sudo pip3 install scipy # FIXME
 
 
 echo "Install the backend services (manually)"
