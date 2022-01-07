5005e61d77388a76fe0730c1635f10dafdf7b862
8cH9azbsFifZ <g@ziegenhain.com>
2022-01-07 01:56:33 +0100

    dissect

--- boards/board.py
+++ boards/board.py
@@ -7,10 +7,7 @@ import json
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
-from tabulate import tabulate 
-""" 
-Use tabulate for an ASCII Hanboard display for debugging purposes
-"""
+
 
 import logging
 logging.basicConfig(level=logging.DEBUG,
@@ -77,41 +74,6 @@ class Board():
             if hold["ImgLayerName"] == hold:
                 return hold["Name"]
         return ""
-
-
-"""
-Example empty board A1 ... C7:
-.......
-.......
-.......  
-Example used holds B1, B7: 4 fingers
-.......
-4.....4
-.......
-"""
-class AsciiBoard(): # TODO continue implementation #82
-    """
-    All stuff for handling an ASCII output of the current hangboard configuration.
-    """
-    def __init__(self, verbose=None):
-        self.set_board_default()
-        self.render_board()
-
-    def set_board_default(self):
-        self.board_row1 = ["A1", "A2", "A3", "A4", "A5", "A6", "A7"] # FIXME
-        self.board_row2 = ["B1", "B2", "B3", "B4", "B5", "B6", "B7"]
-        self.board_row3 = ["C1", "C2", "C3", "C4", "C5", "C6", "C7"]
-
-    def set_active_holds(self):
-        hold1 = "A1" #FIXME 
-        hold2 = "A7"
-        for index, item in enumerate(self.board_row1):
-            if ((item == hold1) or (item == hold2)):
-                self.board_row1[index] = "**"
-            
-    def render_board(self):
-        self.board = (tabulate([self.board_row1, self.board_row2, self.board_row3], tablefmt="grid"))
-        print (self.board)
 
 
 if __name__ == "__main__":
