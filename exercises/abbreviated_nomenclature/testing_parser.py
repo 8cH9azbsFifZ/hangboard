4d7eeac1dfdd27fdc8e94061ec347a02f59e3dbf
Gerolf Ziegenhain <g@ziegenhain.com>
2022-01-16 21:31:21 +0100

    wip

--- exercises/abbreviated_nomenclature/testing_parser.py
+++ exercises/abbreviated_nomenclature/testing_parser.py
@@ -1,22 +1,61 @@
 #!/usr/bin/env python
-import pyparsing as pp
+"""
+[#sets] <#reps> <Exercise> <Hold[left;right]> [Finger] [Grip] [AddedWeight] <HangTime[(Margin)]> <PauseRepTime> [PauseSetTime]
+Examples:
+2x 3x Hang @18mm &4 §Crimp W+5kg 7:3:60s
+2x 7x Hang @Jug 7:3:150
+2x 7x Hang @45mm &Front3 §Open 7:3:150
+2x 7x Hang @45mm &4 §Open 7:3:150
+3x MEDHang @18mm 7(3):180
+3x MAXHang @30mm W+10kg 7(3):180
+"""
+import re
 
-t1 = '2x 3x Hang @25mm +0kg #4;Open 7(3)s:57s' 
-t2=t1 #"1   x y asdf asdf"
-tmp = pp.Word(pp.nums)+"x"+pp.Word(pp.nums)+"x"+pp.Word(pp.alphas)+pp.Optional("@"+pp.Word(pp.alphanums))+pp.Optional("+"+pp.Word(pp.nums)+"kg") \
-    +pp.Optional('#'+pp.Word(pp.alphanums)+";"+pp.Word(pp.alphanums))+pp.Word(pp.nums)+"("+pp.Word(pp.nums)+")s:"+pp.Word(pp.nums)+"s"
+class ExerciseParser():
+    def __init__(self, exercise_string=""):
+        self._exercise_string = exercise_string
 
-x1 = tmp.parse_string(t2)
+        self.Exercise = {}
+        self.Exercise["Sets"] = 1
+        self.Exercise["Reps"] = 1
+        self.Exercise["Type"] = "Hang"
+        self.Exercise["Left"] = "Jug"
+        self.Exercise["Right"] = "Jug"
+        self.Exercise["Finger"] ="4"
+        self.Exercise["Grip"] = "Jug"
+        self.Exercise["AddedWeight"] = "0kg"
+        self.Exercise["Hangtime"] = 7
+        self.Exercise["HangtimeMargin"] = 5
+        self.Exercise["PauseRepTime"] = 53
+        self.Exercise["PauseSetTime"] = 0
 
-print (x1)
-num_sets = x1[0]
-num_reps = x1[2]
-name_exercise = x1[4]
-name_hold = x1[6]
-added_weight = x1[8]
-name_fingers = x1[11]
-name_grip = x1[13]
-time_load = x1[14]
-time_pause = x1[16]
-time_pause_set = x1[18]
-print (num_sets, num_reps, name_exercise, name_hold, added_weight, name_fingers, name_grip, time_load, time_pause, time_pause_set)
+        print (self._exercise_string)
+
+        self._parse()
+
+        print (self.Exercise)
+
+    def _parse(self):
+        pattern = re.compile("^([0-9]+x) ([0-9]+x) ")
+
+        # Parse counters for set and rep
+        if pattern.match (self._exercise_string):
+            print ("Contains a set counter")
+            t = self._exercise_string.split("x")
+            s = t[0]
+            self.Exercise["Sets"] = s
+            r = t[1]
+            self.Exercise["Repts"] = r
+
+        else:
+            r = self._exercise_string.split("x")[0]
+            self.Exercise["Reps"] = r
+    
+
+if __name__ == "__main__":
+    tmp = "2x 3x Hang @18mm &4 §Crimp W+5kg 7:3:60s"
+    e = ExerciseParser(exercise_string=tmp)
+
+
+    tmp = "3x Hang @18mm &4 §Crimp W+5kg 7:3:60s"
+    e = ExerciseParser(exercise_string=tmp)
