a1fc201c6b90d970ed55f065f7c97c63b1403767
8cH9azbsFifZ <g@ziegenhain.com>
2022-01-17 20:02:45 +0100

    wip loop

--- exercises/abbreviated_nomenclature/testing_parser.py
+++ exercises/abbreviated_nomenclature/testing_parser.py
@@ -12,6 +12,9 @@ Examples:
 """
 
 import re
+import logging
+import time
+logging.basicConfig(level=logging.DEBUG, format='Counter(%(threadName)-10s) %(message)s', )
 
 class Exercise():
     def __init__(self):
@@ -63,14 +66,14 @@ class ExerciseParser():
         stmp = self._exercise_string.split()
 
         if pattern.match (self._exercise_string):
-            print ("Contains a set counter")
+            logging.debug ("Contains a set counter")
             i0 = 1
             self.Exercise["Sets"] = int(stmp[0].replace("x",""))
             
         self.Exercise["Reps"] = int(stmp[i0].replace("x",""))
         self.Exercise["Type"] = stmp[i0+1]
         if ";" in stmp[i0+2]:
-            print ("Contains left/right holds")
+            logging.debug ("Contains left/right holds")
             self.Exercise["Left"] = stmp[i0+2].split(";")[0].replace("@","")
             self.Exercise["Right"] = stmp[i0+2].split(";")[1]
         else:
@@ -78,21 +81,21 @@ class ExerciseParser():
             self.Exercise["Right"] = self.Exercise["Left"]
 
         if "&" in stmp[i0+3]:
-            print ("Contains finger")
+            logging.debug ("Contains finger")
             self.Exercise["Finger"] = stmp[i0+3].replace("&","")
 
         if "§" in stmp[i0+4]:
-            print ("Contains grip")
+            logging.debug ("Contains grip")
             self.Exercise["Grip"] = stmp[i0+4].replace("§","")
 
         if "W" in stmp[i0+5]:
-            print ("Contains added weight")
+            logging.debug ("Contains added weight")
             self.Exercise["AddedWeight"] = float(stmp[i0+5].replace("W","").replace("kg",""))
 
         tt = stmp[i0+6].replace("s","").split(":")
     
         if "(" in tt[0]:
-            print ("Contains margin")
+            logging.debug ("Contains margin")
             self.Exercise["Hangtime"] = int(tt[0].split("(")[0].replace("(","").replace(")",""))
             self.Exercise["HangtimeMargin"] = int(tt[0].split("(")[1].replace(")",""))
         else:
@@ -123,76 +126,65 @@ class ExerciseParser():
 
         return self._workout
 
-# FIXME: 1 workout per file
-
 class WorkoutCounter():
     def __init__(self, workout=Workout()):
         self._workout = workout
+        self._sets = self._workout.Workout["Workouts"][0]["Sets"] # FIXME: 1 workout per file
+        self._is_pause = False 
+        self.reset()
 
+    def reset(self):
         self.Counter = {}
         self.Counter["Set"] = 1
         self.Counter["Rep"] = 0
-
-        self.Counter["SetsTotal"] = len(self._workout.Workout["Workouts"][0]["Sets"])
+        self.Counter["SetsTotal"] = len(self._sets)
 
     def __next__(self): 
         if self.Counter["SetsTotal"] == 0:
-            print ("No sets") # FIXME: logging
+            logging.debug ("No sets") 
             return -1
         
-        if self.Counter["Set"] <= self.Counter["SetsTotal"]:
-            self.Counter["RepsTotal"] = self._workout.Workout["Workouts"][0]["Sets"][ self.Counter["Set"]-1 ]["Reps"]
-            if self.Counter["Rep"] < self.Counter["RepsTotal"]:
-                self.Counter["Rep"] = self.Counter["Rep"] + 1
+        if not self._is_pause:  # If exercise (not pause) state
+            self._is_pause = True # toggle state
+            if self.Counter["Set"] <= self.Counter["SetsTotal"]: 
+                self.Counter["RepsTotal"] = self._sets[ self.Counter["Set"]-1 ]["Reps"]
+                self.Counter["SetExercise"] = self._sets[ self.Counter["Set"]-1 ]["Exercise"] 
+                if self.Counter["Rep"] < self.Counter["RepsTotal"]:
+                    self.Counter["Rep"] = self.Counter["Rep"] + 1
+                else:
+                    self.Counter["Rep"] = 1        
+                    self.Counter["Set"] = self.Counter["Set"] + 1
             else:
-                self.Counter["Rep"] = 1        
-                self.Counter["Set"] = self.Counter["Set"] + 1
+                if self.Counter["Rep"] < self.Counter["RepsTotal"]:
+                    self.Counter["Rep"] = self.Counter["Rep"] + 1
+                else:
+                    return -1
+            self.Counter["Exercise"] = self.Counter["SetExercise"]
             return self.Counter
-        else:
-            if self.Counter["Rep"] < self.Counter["RepsTotal"]:
-                self.Counter["Rep"] = self.Counter["Rep"] + 1
-            else:
-                return -1
-        
-        return self.Counter
+        else: # If pause (not exercise) state
+            self._is_pause = False # toggle state
+            self.Counter["Exercise"] = "Pause"
+            return self.Counter
 
 
 class WorkoutLooper():
     def __init__(self, workout=Workout()):
         self._wc = WorkoutCounter(workout=workout)
 
-    
+    def _loop_all(self, stepping=0.1):
+        self._wc.reset()
+        while True:
+            time.sleep(stepping)
+            c = next(self._wc)
+            msg = c
+            if c == -1:
+                break
+            print (msg)
 
 if __name__ == "__main__":
     tmp = "2x 3x Hang @18mm &4 §Crimp W+5kg 7:3:60s"
-    e = ExerciseParser(exercise_string=tmp)
-    e._create_workout_json()
-    #print (e._wjson)
-
     tmp = "2x 3x 4xPullUp @18mm;19mm &4 §Crimp W+5kg 7(2):3:60s"
     e = ExerciseParser(exercise_string=tmp)
-
     f = e._create_workout_json()
-    #print (e._wjson)
-
-    c = WorkoutCounter(workout=f)
-    b = next(c)
-    print (b)
-    b = next(c)
-    print (b)
-    b = next(c)
-    print (b)
-    b = next(c)
-    print (b)    
-    b = next(c)
-    print (b)
-    b = next(c)
-    print (b)
-    b = next(c)
-    print (b)
-    b = next(c)
-    print (b)  
-    b = next(c)
-    print (b)
-    b = next(c)
-    print (b)
+    l = WorkoutLooper(workout=f)
+    l._loop_all()
