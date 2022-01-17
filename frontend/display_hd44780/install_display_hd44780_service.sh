271304dabb27d4b3a51a173202508ba55b1a3ecb
8cH9azbsFifZ <g@ziegenhain.com>
2022-01-17 21:57:25 +0100

    cleanup

--- frontend/display_hd44780/install_display_hd44780_service.sh
+++ frontend/display_hd44780/install_display_hd44780_service.sh
@@ -1,14 +1,8 @@
-#!/bin/bash
-<<<<<<< HEAD:backend/services/install_sensor_force_service.sh
-service="sensor_force.service"
 
-echo "Install hangboard service for: "$service 
-=======
 service="hangboard_display_hd44780.service"
 
 echo "Install hangboard service for: "$service 
 
->>>>>>> dev:frontend/display_hd44780/install_display_hd44780_service.sh
 sudo cp ./$service /lib/systemd/system/
 sudo chmod 644 /lib/systemd/system/$service
 sudo systemctl daemon-reload
