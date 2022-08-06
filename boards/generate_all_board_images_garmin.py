2e131be53013b06fb5b38e66c4ec81483da5774c
8cH9azbsFifZ <g@ziegenhain.com>
2022-08-06 21:46:42 +0200

    fix

--- boards/generate_all_board_images_garmin.py
+++ boards/generate_all_board_images_garmin.py
@@ -5,13 +5,15 @@ import os
 if __name__ == "__main__":
     a = Board()
     svg = SVGBoard(boardname="zlagboard_evo")
-    svg.generate_all_images(holds=a.all_holds) 
+    svg.generate_all_images_garmin(holds=a.all_holds) 
 
     print ("ok") # FIXME: needs imagemagick
     directory = os.fsencode(svg.cachedir)    
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         if filename.endswith(".png"): 
+            print ("Re-Export file "+svg.cachedir+"/"+filename)
+            os.system('inkscape --export-type="png" '+svg.cachedir+"/"+filename)
             print ("Reduce file "+svg.cachedir+"/"+filename)
             os.system("mogrify -geometry 200 "+svg.cachedir+"/"+filename)
             continue
